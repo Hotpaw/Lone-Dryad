fef76f940ca611bb7cf3912bb0105023a493db2a
Sofie Reyes B <sofie.reyes97@gmail.com>
2023-11-20 15:09:47 +0100

    changed follow player and tingz
    scriptet ligger nu i script mappen

--- Assets/Scripts/FollowPlayerScript.cs
+++ Assets/Scripts/FollowPlayerScript.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour
+public class FollowPlayerScript : MonoBehaviour
 {
     public GameObject enemy;
     public Transform target;
     Rigidbody2D rb2D;
-    float enemySpeed = 3f;
+   public float enemySpeed = 3f;
 
     // Start is called before the first frame update
     void Start()
