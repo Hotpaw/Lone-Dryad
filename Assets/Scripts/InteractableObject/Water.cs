47de171ab1283b29fc5d753862d1ae3bddccc3ab
Yeahger1 <andreholton85@gmail.com>
2023-11-20 15:52:05 +0100

    Water

---
+++ Assets/Scripts/InteractableObject/Water.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Water : InteractableObject
+{
+    public float waterAmount;
+
+    public override void Interact()
+    {
+        //Här
+        GameValueManager.INSTANCE.carryingWater += waterAmount;
+        waterAmount = 0;
+    }
+}
