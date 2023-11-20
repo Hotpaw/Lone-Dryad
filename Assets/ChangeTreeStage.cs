5a555fb58021a5f8da1e7ff04d17e4639ff652d4
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-11-20 14:21:40 +0100

    treefab

---
+++ Assets/ChangeTreeStage.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChangeTreeStage : MonoBehaviour
+{
+    public List<Sprite> treeStages;
+    SpriteRenderer spriteRenderer;
+    int id =2;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if(Keyboard.current.spaceKey.wasPressedThisFrame == true)
+        {
+            ChangeTreeSprite(id);
+            id++;
+        }
+    }
+    public void ChangeTreeSprite(int id)
+    {
+        switch (id)
+        {
+            case 2: spriteRenderer.sprite = treeStages[0];
+                break;
+            case 3:
+                spriteRenderer.sprite = treeStages[1];
+                break;
+            case 4:
+                spriteRenderer.sprite = treeStages[2];
+                break;
+        }
+    }
+}
