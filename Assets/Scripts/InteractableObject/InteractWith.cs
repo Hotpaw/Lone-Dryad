37c3105e04f4d8924e25802ef2275eb22cf2ce2c
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-11-20 15:25:26 +0100

    interact

--- Assets/Scripts/InteractableObject/InteractWith.cs
+++ Assets/Scripts/InteractableObject/InteractWith.cs
@@ -5,16 +5,48 @@ using UnityEngine.InputSystem;
 public class InteractWith : MonoBehaviour
 {
     public InteractableObject interactableObject;
-   
+    public bool interactable;
+    bool used = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && Gamepad.current.buttonEast.IsPressed())
+        if (interactable)
         {
-            interactableObject.Interact();
+            if (collision.gameObject.CompareTag("Player") && Gamepad.current.buttonEast.IsPressed() || Keyboard.current.eKey.wasPressedThisFrame == true)
+            {
+                if (!used)
+                {
+                    used = true;
+                    interactableObject.Interact();
+                    DisableInteractable();
+                }
+
+            }
+            else
+            {
+                return;
+            }
         }
         else
         {
-            return;
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                
+                if (!used)
+                {
+                    used = true;
+                    interactableObject.Interact();
+                    DisableInteractable();
+                }
+            }
+            else
+            {
+                return;
+            }
         }
+
+    }
+    public void DisableInteractable()
+    {
+        gameObject.SetActive(false);
     }
 }
