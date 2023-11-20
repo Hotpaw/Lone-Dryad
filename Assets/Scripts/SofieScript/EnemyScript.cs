538fb6b6706eada0e720cad62a2e85a786291206
Sofie Reyes B <sofie.reyes97@gmail.com>
2023-11-20 15:01:16 +0100

    fixed enemy follow script

--- Assets/Scripts/SofieScript/EnemyScript.cs
+++ Assets/Scripts/SofieScript/EnemyScript.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public GameObject enemy;
-    Transform target;
+    public Transform target;
     Rigidbody2D rb2D;
-    float enemySpeed = 2f;
+    float enemySpeed = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
         //Invoke("EnemyFollow", 0.5f);
 
-        EnemyFollow();
+        
 
         rb2D = GetComponent<Rigidbody2D>();
     }
@@ -22,18 +22,21 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetDirection = target.position - transform.position;
+        if (target)
+        {
+            Vector2 targetDirection = target.position - transform.position;
 
-        targetDirection.Normalize();
+            targetDirection.Normalize();
 
-        rb2D.velocity = targetDirection * enemySpeed;
+            rb2D.velocity = targetDirection * enemySpeed;
+
+            transform.right = target.position - transform.position;
+
+        }
 
     }
 
-    public void EnemyFollow()
-    {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-    }
+   
     
 }
 
