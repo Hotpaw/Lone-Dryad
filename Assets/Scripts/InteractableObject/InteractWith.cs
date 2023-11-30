ccc393d6dbef4e0c39b34f6dad829896ea5120cf
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-11-30 11:17:03 +0100

    interactable icons

--- Assets/Scripts/InteractableObject/InteractWith.cs
+++ Assets/Scripts/InteractableObject/InteractWith.cs
@@ -5,14 +5,29 @@ using UnityEngine.InputSystem;
 public class InteractWith : MonoBehaviour
 {
     public InteractableObject interactableObject;
+    public GameObject InteractableIcon;
+    public Sprite[] Icons;
     public bool interactable;
+    public Vector2 iconOffset;
     bool used = false;
     public bool unlimitedUses = false;
+
+    private void Start()
+    {
+        InteractableIcon.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+    private void Update()
+    {
+       // Update the icon to change during runtime whenever a controll is disconnected
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        DisplayInteractableIcon();
+
+      
         if (interactable)
         {
-            
+
             if (collision != null && collision.gameObject.CompareTag("Player") && Gamepad.current.buttonEast.IsPressed() || Keyboard.current.eKey.wasPressedThisFrame == true)
             {
                 if (!used)
@@ -38,7 +53,7 @@ public class InteractWith : MonoBehaviour
         {
             if (collision.gameObject.CompareTag("Player"))
             {
-                
+
                 if (!used)
                 {
                     if (!unlimitedUses)
@@ -58,10 +73,23 @@ public class InteractWith : MonoBehaviour
         }
 
     }
+
+    private void DisplayInteractableIcon()
+    {
+        InteractableIcon.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        InteractableIcon.transform.localPosition = iconOffset;
+        if (Gamepad.current != null) InteractableIcon.gameObject.GetComponent<SpriteRenderer>().sprite = Icons[0];
+        else InteractableIcon.gameObject.GetComponent<SpriteRenderer>().sprite = Icons[1];
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        InteractableIcon.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
     public void DisableInteractable()
     {
         gameObject.SetActive(false);
     }
 
-   
+
 }
