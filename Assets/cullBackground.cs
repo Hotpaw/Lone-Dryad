c7965bef1bd1cc46c7241017d57d40ea78c758e5
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-12-07 12:43:58 +0100

    updated stage 2 cave colliders

--- Assets/cullBackground.cs
+++ Assets/cullBackground.cs
@@ -4,8 +4,42 @@ using UnityEngine;
 
 public class cullBackground : MonoBehaviour
 {
-    public void OnTriggerEnter2D(Collider2D collision)
+    public static cullBackground INSTANCE;
+    public GameObject[] Backgrounds;
+    bool used = false;
+    private void Start()
     {
-        
+        INSTANCE = this;
+    }
+    public void CullingModeCall(int a)
+    {
+        if (!used)
+        {
+
+            StartCoroutine(cullingMode(a));
+        }
+        else return;
+    }
+    public void ChangeBackground(int a)
+    {
+        foreach (var item in Backgrounds)
+        {
+            item.gameObject.SetActive(false);
+        }
+        if (a == 0)
+        {
+            Backgrounds[0].SetActive(true);
+        }
+        if (a == 1)
+        {
+            Backgrounds[1].SetActive(true);
+        }
+    }
+    IEnumerator cullingMode(int a)
+    {
+        used = true;
+        ChangeBackground(a);
+        yield return new WaitForSeconds(0.1f);
+        used = false;
     }
 }
