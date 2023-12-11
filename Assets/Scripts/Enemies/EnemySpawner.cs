c4e9cd65ff7e9b60442074a168316eb288072e28
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-12-11 14:34:00 +0100

    update stage 2 and spawner
    new spawn code.
    new dungeon layout

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -7,21 +7,33 @@ public class EnemySpawner : MonoBehaviour
     public static EnemySpawner INSTANCE;
     public List<GameObject> enemies;
     public Transform[] spawnPoints;
+    public Transform[] destinationPoints;
+   
 
     public void Awake()
     {
-        
+
         INSTANCE = this;
-      
+
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            SpawnEnemy(0, this.transform.position);
+        }
     }
 
     public void SpawnEnemy(int enemyId, Vector2 spawnPosition)
     {
         if (GameValueManager.INSTANCE.treeIsALive)
-            Instantiate(enemies[enemyId], new Vector3(spawnPosition.x, spawnPosition.y, 0), transform.rotation);
+        {
+            GameObject floatingObjectClone = Instantiate(enemies[enemyId], new Vector3(spawnPosition.x, spawnPosition.y, 0), Quaternion.identity);
+            floatingObjectClone.GetComponent<FloatingObject>().floatingTo = destinationPoints[Random.Range(0,destinationPoints.Length)].gameObject;
+        }
     }
     public void SpawnStage2()
     {
-        GameObject clone = Instantiate(enemies[1], spawnPoints[Random.Range(0,spawnPoints.Length)]);
+        GameObject clone = Instantiate(enemies[1], spawnPoints[Random.Range(0, spawnPoints.Length)]);
     }
 }
