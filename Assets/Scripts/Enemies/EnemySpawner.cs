61e448ca053811c97e7ae47fab26fbd0afdf482b
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-12-11 16:08:43 +0100

    stage2 logic

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -8,7 +8,10 @@ public class EnemySpawner : MonoBehaviour
     public List<GameObject> enemies;
     public Transform[] spawnPoints;
     public Transform[] destinationPoints;
-   
+
+    [Header("stage 2")]
+    public Transform[] spawnPoints2;
+    public Transform[] destinationPoints2;
 
     public void Awake()
     {
@@ -18,22 +21,28 @@ public class EnemySpawner : MonoBehaviour
     }
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.G))
-        {
-            SpawnEnemy(0, this.transform.position);
-        }
+      
     }
 
     public void SpawnEnemy(int enemyId, Vector2 spawnPosition)
     {
-        if (GameValueManager.INSTANCE.treeIsALive)
-        {
+       
             GameObject floatingObjectClone = Instantiate(enemies[enemyId], new Vector3(spawnPosition.x, spawnPosition.y, 0), Quaternion.identity);
             floatingObjectClone.GetComponent<FloatingObject>().floatingTo = destinationPoints[Random.Range(0,destinationPoints.Length)].gameObject;
-        }
+        
     }
     public void SpawnStage2()
     {
-        GameObject clone = Instantiate(enemies[1], spawnPoints[Random.Range(0, spawnPoints.Length)]);
+        int rand = Random.Range(0,spawnPoints2.Length);
+        int rand2 = Random.Range(0,destinationPoints2.Length);
+        GameObject clone = Instantiate(enemies[1], spawnPoints[rand]);
+        if(clone.transform.position.x < FindAnyObjectByType<Tree>().gameObject.transform.position.x)
+        {
+            clone.transform.rotation = new Quaternion(0,180,0,0);
+        }
+       
+        clone.GetComponent<FlyingEnemy>().waitAfterSpawn.transform.position = spawnPoints2[rand].position;
+        clone.GetComponent<FlyingEnemy>().target.transform.position = destinationPoints2[rand].position;
+
     }
 }
