e8664df1c9ae8dc77ced39509203ea981afa504b
Yeahger1 <andreholton85@gmail.com>
2023-12-13 16:25:57 +0100

    Stones in stage1

---
+++ Assets/Scripts/InteractableObject/InteractableStone.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InteractableStone : InteractableObject
+{
+    public bool once;
+    public GameObject stone;
+
+    public override void Interact()
+    {
+        if (!once)
+        {
+            GameValueManager.INSTANCE.thePowerToThrowNuts = true;
+            once = true;
+            GameValueManager.INSTANCE.stones++;    
+            Destroy(stone);
+        }
+    }
+}
