40350a2a268e7793f1a00d98bed7d216f32868d1
Sofie Reyes B <sofie.reyes97@gmail.com>
2023-12-19 18:04:53 +0100

    wip trigger 2d lights

---
+++ Assets/TriggerLight2D.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class TriggerLight2D : MonoBehaviour
+{
+    public Light2D Light;
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Light.enabled = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Light.enabled = false;
+        }
+    }
+}
