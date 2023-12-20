c9becbae615c9203145bea4a8994683fad0ae173
Yeahger1 <andreholton85@gmail.com>
2023-12-20 15:13:49 +0100

    Update InteractableGnome.cs

--- Assets/Scripts/InteractableObject/InteractableGnome.cs
+++ Assets/Scripts/InteractableObject/InteractableGnome.cs
@@ -6,16 +6,16 @@ public class InteractableGnome : InteractableObject
 {
     public override void Interact()
     {
-        PopUpText.INSTANCE.PopUpMessage("The storm is coming!", Color.red, 12);
+        PopUpText.INSTANCE.PopUpMessage("The storm is coming!", Color.red, 12, PopUpText.Icon.Gnome);
         StartCoroutine(TalkingGnome());
     }
 
     private IEnumerator TalkingGnome()
     {
         yield return new WaitForSeconds(4);
-        PopUpText.INSTANCE.PopUpMessage("Your tree will die in a short while if you do not repair the crystall", Color.red);
+        PopUpText.INSTANCE.PopUpMessage("Your tree will die in a short while if you do not repair the crystall", Color.red, 3, PopUpText.Icon.Gnome);
         yield return new WaitForSeconds(4);
-        PopUpText.INSTANCE.PopUpMessage("Bring me seven crystallpieces, and i can help you!", Color.red);
+        PopUpText.INSTANCE.PopUpMessage("Bring me seven crystallpieces, and i can help you!", Color.red, 3, PopUpText.Icon.Gnome);
     }
 }
 
