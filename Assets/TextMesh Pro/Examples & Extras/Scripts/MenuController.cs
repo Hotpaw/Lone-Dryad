1569a26ef59ed83e7ee5bcf300430b27d8d2fc5f
Sofie Reyes B <sofie.reyes97@gmail.com>
2023-12-20 18:09:36 +0100

    wip menu toggling

--- Assets/TextMesh Pro/Examples & Extras/Scripts/MenuController.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/MenuController.cs
@@ -1,39 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR.Haptics;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
 
     //[SerializeField] private MenuController m_MenuController;
-    [SerializeField] public InputActionReference menuButton;
+    //[SerializeField] public InputActionReference menuButton;
     public GameObject menuWindow;
     private bool menuOpen = true;
-    
-    // Start is called before the first frame update
-    void Start()
+    private InputAction playerNavigation;
+
+    public List<Button> menuButtons = new List<Button>();
+
+    public InputActionMap playerNavigationMap;
+
+    public void Awake()
     {
-        menuButton.action.started += MenuToggle;
+        //Button[] menuButtons = FindObjectByType<Button>();
+        menuButtons.AddRange(menuButtons);
+        playerNavigation = new InputAction("Player/MenuNavigation");
+
+        playerNavigation.performed += ctx => OnNavigationInput(ctx.ReadValue<Vector2>()); //ctx är callbackcontext
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
+        playerNavigation.Enable();
+    }
+
+   private void OnDisable() 
+   {
+    playerNavigation?.Disable();
+   }
+    private void OnNavigationInput(Vector2 inputVector)
+    {
+        float verticalInput = inputVector.y;
+
+
+        if (verticalInput > 0) 
+        {
+            //navigera up
+            Navigate(-1);
         
-    }
+        }
+        else if (verticalInput < 0) 
+        {
+          Navigate(1);
+        
+          //navigera ner 
+        }
 
-    private void MenuToggle(InputAction.CallbackContext obj) 
-    {
-    
-      menuOpen = !menuOpen;
-      menuWindow.SetActive(menuOpen);
-    
     }
-
-    private void OnDestroy()
+    private void Navigate(int direction)
     {
-        menuButton.action.started -= MenuToggle;
+        int currentButtonIndex = GetCurrentButtonIndex(); //för att veta vilken knapp via dess index
+        menuButtons[currentButtonIndex].Deselect (); //deselect nuvarande knapp
+        int nextIndex = (currentButtonIndex + direction + menuButtons.Count); //beräkna nästa knapp index baserat på riktningen 
+        menuButtons[nextIndex].Select(); //välj knappen
+    }
+    private int GetCurrentButtonIndex()
+    {
+        int buttonIndex = 0;
+        return buttonIndex;
     }
 }
