6dae1f446c55089e7c89cd777718fb520a1d50f4
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2023-12-20 11:50:15 +0100

    update stage 5 and interface
    Fix Icons are now only displayed depending on their type.
    Change this enum in InteractWith object in inspector
    -Normal typ - Always displayed
    -Tree - only displayed when you have gained water or won stage
    -Plant - only able to rplant when you have the power to plant.

--- Assets/PlantSeed.cs
+++ Assets/PlantSeed.cs
@@ -6,7 +6,12 @@ public class PlantSeed : InteractableObject
 {
     public GameObject PlantPart;
     bool usable = true;
+    public Animator animator;
 
+    public enum Type { event1, grow, crystal };
+    public Type type;
+
+    bool eventUsed = false;
     void Start()
     {
 
@@ -17,19 +22,27 @@ public class PlantSeed : InteractableObject
     }
     public override void Interact()
     {
-        if (usable)
+        if (GameValueManager.INSTANCE.thePowerToPlant)
         {
-           
 
-           
-            PlantPart.GetComponent<Animator>().Play("Grow");
-            StartCoroutine(Decay());
+            if (type == Type.event1 && !eventUsed)
+            {
+                eventUsed = true;
+                animator.SetTrigger("Event");
+
+            }
+            if (usable)
+            {
+                PlantPart.GetComponent<Animator>().Play("Grow");
+                StartCoroutine(Decay());
+            }
         }
+
     }
     IEnumerator Decay()
     {
         usable = false;
-       
+
         yield return new WaitForSeconds(3);
         PlantPart.GetComponent<Animator>().Play("Ungrow");
         usable = true;
---
+++ Assets/activatePlant.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activatePlant : InteractableObject
+{
+    public override void Interact()
+    {
+        GameValueManager.INSTANCE.thePowerToPlant = true;
+        PopUpText.INSTANCE.PopUpMessage("You gained the power to regrow plants",Color.black,5f);
+        transform.GetComponentInParent<GameObject>().SetActive(false);
+    }
+
+    
+}
