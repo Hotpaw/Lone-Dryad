dee39c00f91570b3fb55723faa904d2dc6a98aa5
Sofie Reyes B <sofie.reyes97@gmail.com>
2023-12-21 16:32:25 +0100

    wip menu gamepad updates

--- Assets/TextMesh Pro/Examples & Extras/Scripts/MenuController.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/MenuController.cs
@@ -13,62 +13,109 @@ public class MenuController : MonoBehaviour
 
     //[SerializeField] private MenuController m_MenuController;
     //[SerializeField] public InputActionReference menuButton;
-    public GameObject menuWindow;
-    private bool menuOpen = true;
-    private InputAction playerNavigation;
+    //public GameObject menuWindow;
+    //private bool menuOpen = true;
 
     public List<Button> menuButtons = new List<Button>();
 
-    public InputActionMap playerNavigationMap;
+    public PlayerInput playerNavigation;
+
+    int selectIndex = 0;
 
     public void Awake()
     {
-        //Button[] menuButtons = FindObjectByType<Button>();
-        menuButtons.AddRange(menuButtons);
-        playerNavigation = new InputAction("Player/MenuNavigation");
-
-        playerNavigation.performed += ctx => OnNavigationInput(ctx.ReadValue<Vector2>()); //ctx är callbackcontext
-    }
-
-
-    private void OnEnable()
-    {
-        playerNavigation.Enable();
-    }
-
-   private void OnDisable() 
-   {
-    playerNavigation?.Disable();
-   }
-    private void OnNavigationInput(Vector2 inputVector)
-    {
-        float verticalInput = inputVector.y;
-
-
-        if (verticalInput > 0) 
-        {
-            //navigera up
-            Navigate(-1);
         
-        }
-        else if (verticalInput < 0) 
-        {
-          Navigate(1);
-        
-          //navigera ner 
-        }
 
     }
-    private void Navigate(int direction)
-    {
-        int currentButtonIndex = GetCurrentButtonIndex(); //för att veta vilken knapp via dess index
-        menuButtons[currentButtonIndex].Deselect (); //deselect nuvarande knapp
-        int nextIndex = (currentButtonIndex + direction + menuButtons.Count); //beräkna nästa knapp index baserat på riktningen 
-        menuButtons[nextIndex].Select(); //välj knappen
-    }
-    private int GetCurrentButtonIndex()
-    {
-        int buttonIndex = 0;
-        return buttonIndex;
-    }
+
+
+   // private void OnEnable()
+   // {
+   //    playerNavigation.Enable();
+   // }
+
+   //private void OnDisable() 
+   //{
+   // playerNavigation.Disable();
+   //}
+
+
+
+
+
+
+
+
+
+
+
+    //Button[] foundButtons = FindObjectsOfType<Button>();
+    //menuButtons.AddRange(menuButtons);
+    //playerNavigation.performed += ctx => OnNavigationInput(ctx.ReadValue<Vector2>()); //ctx är callbackcontext
+    ////playerNavigation = new InputAction("Player/MenuNavigation");
+    ////playerNavigation.AddBinding("<Gamepad>/LeftStick/Up"); 
+    ////playerNavigation.AddBinding("<Gamepad>/LeftStick/Down"); 
+
+
+    //private void MoveSelection(float direction)
+    //{
+    //    int buttonCount = menuButtons.Count; //riktning up ör positiv, riktning ner negativ
+    //    menuButtons[selectIndex].OnDeselect(null); //deselect kallad för säga till ui att den förra knappen ej längre är vald
+
+    //    selectIndex =(selectIndex + buttonCount + (int) direction) % buttonCount; //uppdatera nya index baserad på riktning
+    //    menuButtons[selectIndex].OnSelect(null); //välj den nya highlightade knappen. 
+
+    //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //private void OnNavigationInput(Vector2 inputVector)
+    //{
+    //    float verticalInput = inputVector.y;
+
+
+    //    if (verticalInput > 0) 
+    //    {
+    //        //navigera up
+    //        Navigate(-1);
+
+    //    }
+    //    else if (verticalInput < 0) 
+    //    {
+    //      Navigate(1);
+
+    //      //navigera ner 
+    //    }
+
+    //}
+    //private void Navigate(int direction)
+    //{
+    //    int currentButtonIndex = GetCurrentButtonIndex(); //för att veta vilken knapp via dess index
+    //    menuButtons[currentButtonIndex].Deselect (); //deselect nuvarande knapp
+    //    int nextIndex = (currentButtonIndex + direction + menuButtons.Count); //beräkna nästa knapp index baserat på riktningen 
+    //    menuButtons[nextIndex].Select(); //välj knappen
+    //}
+    //private int GetCurrentButtonIndex()
+    //{
+    //    int buttonIndex = 0;
+    //    return buttonIndex;
+    //}
 }
