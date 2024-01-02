6311ad386e6838da7e957ae1928005a5d25fe140
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2024-01-02 10:17:43 +0100

    update stage 5

--- Assets/Scripts/States/TreeStateMachine/TreeState5.cs
+++ Assets/Scripts/States/TreeStateMachine/TreeState5.cs
@@ -1,26 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TreeState5 : State
 {
     public static TreeState5 INSTANCE;
     bool once;
-
+    public Animator wereWolfEyes;
     public void Start()
     {
-        PopUpText.INSTANCE.PopUpMessage("The water is Gone, I can climb the tree branches to check it out", Color.black,5f);
+        PopUpText.INSTANCE.PopUpMessage("The water is Gone, i should climb the branches to find out what happened", Color.black,5f);
         INSTANCE = this;
       
     }
     public override State RunCurrentState()
     {
         
-        if(GameValueManager.INSTANCE.numberOfCrystallPieces == 4)
-        {
-            GameValueManager.INSTANCE.nextLevelAvailable = true;
-        }
+        
             return this;
      
+    }
+    public void ActivateWereWolfEvent()
+    {
+        wereWolfEyes.SetTrigger("EyeTrigger");
+        SceneManager.LoadScene(GameValueManager.INSTANCE.currentsceneBuildIndex + 1);
+    }
+    public void NextLevel()
+    {
+        GameValueManager.INSTANCE.nextLevelAvailable = true;
     }
 }
