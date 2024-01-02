7873d273934370a15c3033d13cd4747db9244540
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2024-01-02 10:41:58 +0100

    update

--- Assets/Scripts/States/TreeStateMachine/TreeState5.cs
+++ Assets/Scripts/States/TreeStateMachine/TreeState5.cs
@@ -8,26 +8,44 @@ public class TreeState5 : State
     public static TreeState5 INSTANCE;
     bool once;
     public Animator wereWolfEyes;
+   public GameObject caveleft;
     public void Start()
     {
-        PopUpText.INSTANCE.PopUpMessage("The water is Gone, i should climb the branches to find out what happened", Color.black,5f);
+        PopUpText.INSTANCE.PopUpMessage("The water is Gone, i should climb the branches to find out what happened", Color.black, 5f);
         INSTANCE = this;
-      
+
     }
     public override State RunCurrentState()
     {
-        
-        
-            return this;
-     
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ActivateWereWolfEvent();
+        }
+        return this;
+
     }
     public void ActivateWereWolfEvent()
     {
-        wereWolfEyes.SetTrigger("EyeTrigger");
-        SceneManager.LoadScene(GameValueManager.INSTANCE.currentsceneBuildIndex + 1);
+        StartCoroutine(WereWolfEvent());
     }
     public void NextLevel()
     {
         GameValueManager.INSTANCE.nextLevelAvailable = true;
+    }
+    IEnumerator WereWolfEvent()
+    {
+        blackOut blackout = FindObjectOfType<blackOut>();
+        blackout.startBlackOut = true;
+        yield return new WaitForSeconds(3);
+        cameraFollow cam = FindObjectOfType<cameraFollow>();
+        cam.transform.position = wereWolfEyes.gameObject.transform.position;
+        caveleft.SetActive(true);
+       
+        wereWolfEyes.SetTrigger("EyeTrigger");
+        blackout.startBlackOut = true;
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(GameValueManager.INSTANCE.currentsceneBuildIndex + 1);
+
     }
 }
