e51a53d3ca4c02c1f93901b172ac263681b19e61
Yeahger1 <andreholton85@gmail.com>
2024-01-03 16:15:38 +0100

    Start scene 3

--- Assets/Scripts/InteractableObject/InteractableGnome.cs
+++ Assets/Scripts/InteractableObject/InteractableGnome.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 
 public class InteractableGnome : InteractableObject
 {
+    public Animator animator;
     
     public override void Interact()
     {
         if (GameValueManager.INSTANCE.thePowerToPickUpCrystals == false)
         {
+            animator.SetBool("Talking", true);
             GameValueManager.INSTANCE.stormStrenght++;
-            GameValueManager.INSTANCE.thePowerToPickUpCrystals = true;
+            
+
             PopUpText.INSTANCE.PopUpMessage("The storm is coming!", Color.red, 12, PopUpText.Icon.Gnome);
             StartCoroutine(TalkingGnome());
         }
-        if (GameValueManager.INSTANCE.numberOfCrystallPieces >= 7)
-        {            
+        else if (GameValueManager.INSTANCE.thePowerToPickUpCrystals == true && GameValueManager.INSTANCE.numberOfCrystallPieces < 7)
+        {
+            animator.SetBool("Talking", true);
+            PopUpText.INSTANCE.PopUpMessage("Why are you wasting time here? You have to find " 
+                + (7 - GameValueManager.INSTANCE.numberOfCrystallPieces) + " more crystal pieces!", Color.red, 5, PopUpText.Icon.Gnome);
+            StartCoroutine(StopTalkingGnome());
+        }
+        else if (GameValueManager.INSTANCE.numberOfCrystallPieces >= 7)
+        {
+            animator.SetBool("Talking", true);
             PopUpText.INSTANCE.PopUpMessage("I will now repair the crystal for you!", Color.red, 5, PopUpText.Icon.Gnome);
             GameValueManager.INSTANCE.theShieldIsActive = true;
             GameValueManager.INSTANCE.stormStrenght += 2;
@@ -28,11 +39,19 @@ public class InteractableGnome : InteractableObject
     private IEnumerator TalkingGnome()
     {
         yield return new WaitForSeconds(3);
+        GameValueManager.INSTANCE.thePowerToPickUpCrystals = true;
         PopUpText.INSTANCE.PopUpMessage("Your tree will die in a short while if you do not repair the crystall", Color.red, 3, PopUpText.Icon.Gnome);
         yield return new WaitForSeconds(4);
         PopUpText.INSTANCE.PopUpMessage("Bring me seven crystallpieces, and i will help you!", Color.red, 3, PopUpText.Icon.Gnome);
+        animator.SetBool("Talking", false);
 
     }
+    private IEnumerator StopTalkingGnome()
+    {
+        yield return new WaitForSeconds(3);
+        animator.SetBool("Talking", false);
+    }
+
 }
 
 
--- Assets/Scripts/environment/BuildingCrystal.cs
+++ Assets/Scripts/environment/BuildingCrystal.cs
@@ -11,36 +11,40 @@ public class BuildingCrystal : MonoBehaviour
     public float buildTimer;
     public int i;
     
-    public void BuildCrystal()
-    {
-        //spriteRenderer.sprite = crystalStates[i];
-        //buildTimer += Time.deltaTime;
-        //if (buildTimer < 1.5f)
-        //{
-        //    i = 0;            
-        //    particle.Play();
-        //}
-        //else if (buildTimer < 3)
-        //{
-        //    i = 1;
-        //    particle.Play();
-        //}
-        //else if (buildTimer < 4.5)
-        //{
-        //    i = 2;
-        //    particle.Play();
-        //}
-        //else if (buildTimer < 6)
-        //{
-        //    i = 3;
-        //    particle.Play();
-        //}
-        //else if (buildTimer < 7.5)
-        //{
-        //    i = 4;
-        //    particle.Play();
-        //}
-    }
+    //public void BuildCrystal()
+    //{
+    //    //spriteRenderer.sprite = crystalStates[i];
+    //    //buildTimer += Time.deltaTime;
+    //    //if (buildTimer < 1.5f)
+    //    //{
+    //    //    i = 0;
+            
+    //    //}
+    //    //else if (buildTimer < 3)
+    //    //{
+    //    //    i = 1;
+            
+    //    //}
+    //    //else if (buildTimer < 4.5)
+    //    //{
+    //    //    i = 2;
+            
+    //    //}
+    //    //else if (buildTimer < 6)
+    //    //{
+    //    //    i = 3;
+            
+    //    //}
+    //    //else if (buildTimer < 7.5)
+    //    //{
+    //    //    i = 4;
+            
+    //    //}
+    //    //if (buildTimer % 1.5 == 0)
+    //    //{
+    //    //    ParticlePlay();
+    //    //}
+    //}
     public void ParticlePlay()
     {
         particle.Play();
