eef88b64a67c4a2738efc9a8ce36e7bd88c17ca1
Hotpaw <94328693+Hotpaw@users.noreply.github.com>
2024-01-04 22:23:19 +0100

    update scen1

--- Assets/Scripts/Character/projectile.cs
+++ Assets/Scripts/Character/projectile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class projectile : MonoBehaviour
 {
     float timer;
-    float cooldown = 0.6f;
+    float cooldown = 1f;
     public int damage;
     private void Update()
     {
