7fc408beb83c4bf2551eee837e0c7aec7dd67ee6
Yeahger1 <andreholton85@gmail.com>
2024-01-04 12:14:44 +0100

    Scene 3 intro

--- Assets/Scripts/States/TreeStateMachine/TreeState4.cs
+++ Assets/Scripts/States/TreeStateMachine/TreeState4.cs
@@ -7,6 +7,14 @@ public class TreeState4 : State
     public static TreeState4 INSTANCE;
     bool once;
 
+    //Intro
+    public GameObject cameraPoint;
+    public cameraFollow cameraFoll;
+    public GameObject player;
+    public bool cameraZoomOut;
+    public float cameraZoomScale;
+    public float brokenShieldTimer;
+
     public void Awake()
     {
         if (INSTANCE != null) Destroy(this.gameObject);
@@ -15,7 +23,21 @@ public class TreeState4 : State
     }
     public void Update()
     {
-        
+        brokenShieldTimer += Time.deltaTime;
+        cameraFoll.player = GameObject.FindGameObjectWithTag("CameraPoint").transform;
+
+        if (cameraZoomScale > 11f)
+        {
+            cameraZoomScale -= 2 * Time.deltaTime;
+            cameraFoll.cameraDistance = cameraZoomScale;
+            if (cameraZoomScale < 17)
+            {
+                cameraPoint.transform.position = Vector3.MoveTowards(cameraPoint.transform.position, player.transform.position, 5 * Time.deltaTime);
+            }
+        }
+        else
+            cameraFoll.player = player.transform;
+
     }
     public override State RunCurrentState()
     {
